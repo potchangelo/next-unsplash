b63673e647108487e43b13a211d517c0abc27a49
Potchara Puttawanchai <potchangelo@gmail.com>
2020-08-18 13:53:49 +0700

    Download photo by fetch

--- pages/api/download.js
+++ pages/api/download.js
@@ -1,32 +1,23 @@
-import https from 'https';
 import { getPhoto } from '../../api';
 
 export default async function(req, res) {
-    const uid = 'ky5hr4t02f';
-    let photo = null;
-    let buffers = [];
+    const uid = 'ky5hr4t02f14658';
     try {
-        photo = await getPhoto(null, uid);
-        https.get(photo.url.large, res34 => {
-            res34.on('data', chunk => {
-                buffers.push(chunk)
-            })
-            res34.on('end', () => {
-                const image = Buffer.concat(buffers);
-                res.setHeader("Content-Disposition", "attachment; filename=pikachu.jpg");
-                res.setHeader('Content-Type', 'image/jpeg');
-                res.setHeader('Content-Length', image.byteLength);
-                res.status(200);
-                res.send(image);
-            })
-        })
-        // res.setHeader("Content-Disposition", "attachment; filename=pikachu.jpg");
-        // res.setHeader('Content-Type', 'image/jpeg');
-        // res.status(200);
+        const photo = await getPhoto(null, uid);
+        const fetchedRes = await fetch(photo?.url?.large);
+        const fetchedData = await fetchedRes.arrayBuffer();
+        const basedFilename = `${photo.user.displayName}-${photo.uid}-unsplash-cloned.jpg`;
+        const filename = basedFilename.replace(' ', '-').toLowerCase();
+        res.setHeader("Content-Disposition", `attachment; filename=${filename}`);
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.setHeader('Content-Length', fetchedData.byteLength);
+        res.status(200);
+        const imageBuffer = Buffer.from(fetchedData);
+        res.send(imageBuffer);
     }
     catch (error) {
-        console.log(error);
+        console.log(error.toString());
+        res.status(404);
+        res.send('Cannot find image');
     }
-    
-    // res.end('2');
 }
