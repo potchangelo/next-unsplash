8ba9f9be133cb0ff0190022b3a65d091fce8a3b9
Potchara Puttawanchai <potchangelo@gmail.com>
2020-08-24 12:04:31 +0700

    Remove console.log

--- api/_photos.js
+++ api/_photos.js
@@ -1,21 +1,18 @@
 async function getPhotos(key, beforeId) {
     let url = 'http://localhost:8080/photos';
     if (!!beforeId) url += `?beforeId=${beforeId}`;
-    console.log(url)
     const res = await fetch(url);
     return await res.json();
 }
 
 async function getPhoto(key, uid) {
     const url = `http://localhost:8080/photos/${uid}`;
-    console.log(url)
     const res = await fetch(url);
     return await res.json();
 }
 
 async function getRandomPhoto(key) {
     const url = `http://localhost:8080/photos/random`;
-    console.log(url)
     const res = await fetch(url);
     return await res.json();
 }
--- api/_users.js
+++ api/_users.js
@@ -1,7 +1,6 @@
 async function getUser(key, username, includedLatestPhotos = true) {
     let url = `http://localhost:8080/users/${username}`;
     if (!!includedLatestPhotos) url += `?latestPhotos=1`;
-    console.log(url);
     const res = await fetch(url);
     return await res.json();
 }
@@ -9,14 +8,12 @@ async function getUser(key, username, includedLatestPhotos = true) {
 async function getUserPhotos(key, username, beforeId) {
     let url = `http://localhost:8080/users/${username}/photos`;
     if (!!beforeId) url += `?beforeId=${beforeId}`;
-    console.log(url);
     const res = await fetch(url);
     return await res.json();
 }
 
 async function getRandomUsers() {
     const url = 'http://localhost:8080/users/';
-    console.log(url);
     const res = await fetch(url);
     return await res.json();
 }
