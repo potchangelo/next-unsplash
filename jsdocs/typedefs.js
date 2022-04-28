3c2453f483aa67d7ff734fa8beb0a96be8da5030
Potchara Puttawanchai <potchangelo@gmail.com>
2022-04-28 18:19:33 +0700

    Add JSDoc typedefs file

---
+++ jsdocs/typedefs.js
@@ -0,0 +1,24 @@
+// --- Models
+/**
+ * @typedef {object} Topic
+ * @property {string} uid
+ * @property {string} slug
+ * @property {string} title
+ * @property {string} description
+ * @property {boolean} isFeatured
+ * @property {Date} createdAt
+ * @property {Date} updatedAt
+ * @property {TopicCoverUrl} coverUrl
+ */
+
+/**
+ * @typedef {object} TopicCoverUrl
+ * @property {string} small
+ * @property {string} medium
+ * @property {string} large
+ * @property {string} creditUser
+ * @property {string} creditUserLink
+ * @property {string} creditPhotoLink
+ */
+
+exports.unused = {};
